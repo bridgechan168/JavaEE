39e0fb10df8e78be6e9b4156c1dae9245fc88ce4
318172 <d>
2018-11-23 10:44:58 +0800

    add

---
+++ ServletSample/src/com/bridge/javaee/Demo2.java
@@ -0,0 +1,46 @@
+package com.bridge.javaee;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo2
+ */
+@WebServlet("/Demo2")
+public class Demo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext context = this.getServletContext();
+		context.setAttribute("myName", "Bridge Chan");
+		String strGlobalName = context.getInitParameter("globalName");
+		response.getWriter().append("Served at: " + strGlobalName).append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
